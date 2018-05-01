35f4c60625516d8142bc27d38093ed5647cdae23
Sam Mayer <samuel.a.mayer@outlook.com>
2018-05-01 15:14:33 -0700

    Add spring annotations

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -3,6 +3,9 @@ package io.pivotal.pal.tracker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,12 +20,14 @@ public class TimeEntryController {
         this.localRepo = repo;
     }
 
+    @PostMapping("/time/create")
     public ResponseEntity create(TimeEntry timeEntryToCreate) {
         this.localRepo.create(timeEntryToCreate);
 
         return new ResponseEntity(timeEntryToCreate, HttpStatus.CREATED);
     }
 
+    @GetMapping("/time/read")
     public ResponseEntity read(long l) {
 
         TimeEntry t = this.localRepo.find(l);
@@ -32,10 +37,12 @@ public class TimeEntryController {
         return new ResponseEntity(HttpStatus.NOT_FOUND);
     }
 
+    @GetMapping("/time/list")
     public ResponseEntity<List<TimeEntry>> list() {
         return new ResponseEntity(this.localRepo.list(), HttpStatus.OK);
     }
 
+    @PostMapping("/time/update")
     public ResponseEntity update(long l, TimeEntry expected) {
         TimeEntry temp = this.localRepo.update(l, expected);
         if(temp == null){
@@ -44,6 +51,7 @@ public class TimeEntryController {
         return new ResponseEntity(temp, HttpStatus.OK);
     }
 
+    @DeleteMapping("/time/delete")
     public ResponseEntity<TimeEntry> delete(long l) {
 
         this.localRepo.delete(l);
