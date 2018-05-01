6e6b10872c1b42c247b9fd52eb1e7163d9ce752b
Sam Mayer <samuel.a.mayer@outlook.com>
2018-05-01 09:50:19 -0700

    Update .yml for deployment, begin Spring MVC work

---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntry.java
@@ -0,0 +1,11 @@
+package io.pivotal.pal.tracker;
+
+import java.time.LocalDate;
+
+public class TimeEntry {
+
+    private long id, projectId, userId;
+    private LocalDate date;
+    private int hours;
+
+}
